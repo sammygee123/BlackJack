7bd793171e37b5d853606c362dd00b17729ebcd3
Sammy Gransden <108677712+sammygee123@users.noreply.github.com>
2022-08-27 22:45:01 +0100

    made button and sum active

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ let secondCard = 11
 let sum = firstCard + secondCard + 4
 let hasBlackJack = false
 let isAlive = true
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
+    sumEl.textContent = "sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
-    console.log(message)    
+    messageEl.textContent = message
 }
